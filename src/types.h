dc561b5e379abd0fb0d2dfec5b3abf1721b772a1
hitalloazevedo <azevedohitallo@gmail.com>
2024-10-23 17:18:22 -0300

    struct unica

--- src/types.h
+++ src/types.h
@@ -4,20 +4,10 @@
 #define MAX_EMAIL 50
 #define MAX_PASSWORD 20
 
-typedef struct
-{
-  char email[MAX_EMAIL];
-  char password[MAX_PASSWORD];
-} User;
-
-typedef struct {
-  char * email;
-  char * password;
-} UserCriptographed;
-
-typedef struct {
-  UserCriptographed * UsersCriptographed;
-  int size;
-} UserCriptographedArray;
+typedef struct { 
+  char servico[40];
+  char email[40];
+  char password[40];
+} Credential;
 
 #endif
