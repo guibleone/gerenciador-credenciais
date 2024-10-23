fbb10f50b20a130fea54816c0a99fb2a1506bf66
hitalloazevedo <azevedohitallo@gmail.com>
2024-10-23 17:18:45 -0300

    separando responsabilidades

--- src/main.c
+++ src/main.c
@@ -3,17 +3,9 @@
 
 #include "funcs.h"
 
-int main(int argc, char *arcv[])
-{
-  User *user = (User *)malloc(sizeof(User));
-  FILE *registro = fopen("usuarios.txt", "a+");
-  if (registro == NULL)
-  {
-    fprintf(stderr, "Erro: Falha ao abrir registro de usuários.\n");
-    exit(EXIT_FAILURE);
-  }
+int main(int argc, char *arcv[]){
 
-  //login
+  //sistema de login
 
   printf("BEM VINDO AO ENCRIPTADOR 3000\n"); 
   printf("Oque deseja fazer:\n"); 
@@ -33,21 +25,16 @@ int main(int argc, char *arcv[])
       handleCreateNewCredential();
       break;
     case 3:
-      updateUser(user, registro);
+      // atualizar credencial
       break;
     case 4:
-      deleteUser(user, registro);
+      // deletar credencial
       break;
     default: 
       printf("Não selecionado nenhuma opcão");
       return 0;
       break;
   }
-
-  // printUser(user);
-
-  fclose(registro);
-  free(user);
 
   return EXIT_SUCCESS;
 }
