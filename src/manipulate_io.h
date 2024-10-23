75cd6242f6c710a67fe260ef0f55cad82ac9ce8d
hitalloazevedo <azevedohitallo@gmail.com>
2024-10-23 17:19:02 -0300

    removendo arquivos desnecessarios

--- src/manipulate_io.h
+++
@@ -1,8 +0,0 @@
-#ifndef MANIPULATE_IO_H
-#define MANIPULATE_IO_H
-
-void appendFile();
-
-char* toCriptograph (char * string); 
-int compareUnsignedChar (unsigned char * text1,unsigned char * text2, int nBytes);
-#endif
